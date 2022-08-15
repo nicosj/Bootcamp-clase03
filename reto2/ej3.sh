d1d5c6fe85b6be6277ad61fcec23365768b88d1f
nicosj <nico.river09.9@gmail.com>
2022-08-15 00:26:53 -0300

    Linux v2

--- reto2/ej3.sh
+++ reto2/ej3.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mod=(" " " o+r " " u-w " " a-x " " go-r " " g-r,o+wx " " u+w,o-r " " u+w,g+x,o-x,o+w " " u-rw,g-r " " u+rw,g+rw,o+r " )
+mod=("" " o+r " " u-w " " a-x " " go-r " " g-r,o+wx " " u+w,o-r " " u+w,g+x,o-x,o+w " " u-rw,g-r " " u+rw,g+rw,o+r " )
 
 for i in ${!mod[@]}
 do
+	if [ -n ${mod[$i]} ]
+	then
 	chmod ${mod[$i]} archiv$i
+	fi
 done
 
